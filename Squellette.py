39ddd15f68ff5e30764d786a4b1dc43814a02859
WalterxWhite <guilain.savry@outlook.com>
2015-11-17 09:07:10 +0100

    Update Squellette.py

--- Squellette.py
+++ Squellette.py
@@ -1,4 +1,10 @@
 " I partie : Transformer les valeurs audio des fichiers en valeurs numérique "
 
+  " Lire les differents fichier audio à la chaine "
+  
+  " Utiliser le module AIFC sur les fichiers "
+  
+  " Enregistrer le tout dans un tableau "
+
 
 " II partie : "
