cbe2449a1b3b608a2d2da2085d7c289ca80796ff
ThibaultGosselin <thibault.gosselin@live.fr>
2015-12-04 09:58:22 +0100

    Update Trie_baleine.py

--- Trie_baleine.py
+++ Trie_baleine.py
@@ -15,3 +15,31 @@ def Trie (csv):
       i+=1
 
   baleine_franche = csv.writer(open("Baleine_franche.csv", "wb"))
+import numpy as np
+import csv
+
+def trie (file):
+  file = 'train.csv'
+  data = np.opentxt(file)
+  clip_name = data[:,0]
+  label = data[:,1]
+  taille=len(clip_name)
+  k=0
+  for i in range (taille):
+    baleine_franche[]
+    if label[i]==1:
+      baleine_franche[k]=clip_name[i]
+      k+=1
+    else : 
+      i+=1
+  baleine_franche = csv.writer(open("Baleine_franche.csv", "wb"))
+
+  return baleine_franche
+
+
+def main ():
+
+  trie(file)
+
+if __name__=='__main__' : 
+   main()
