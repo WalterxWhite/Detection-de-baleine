df320249ac0b33f4934873eff649562fc7b49e38
WalterxWhite <guilain.savry@outlook.com>
2015-12-04 08:55:16 +0100

    Update Trie_baleine.py

--- Trie_baleine.py
+++ Trie_baleine.py
@@ -3,10 +3,19 @@ import matplotlib.pyplot as plt
 from scipy import stats
 from math import sqrt
 from sklearn import linear_model
+import csv
 
-def ouverture ( ):
+def Trie (csv):
   csv = np.genfromtxt ('train.csv', delimiter=",")
-  clip_name = csv[:,0]
-  label = csv[:,1]
-
+  clip_name = csv[1:,0]
+  label = csv[1:,1]
+  taille=len(clip_name)
   
+  for i in range (taille):
+    if label[i]==1:
+      baleine_franche[k]=clip_name[i]
+      k+=1
+    else : 
+      i++
+
+  baleine_franche = csv.writer(open("Baleine_franche.csv", "wb"))
