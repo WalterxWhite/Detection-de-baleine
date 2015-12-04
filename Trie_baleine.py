463bb5b0a8e3b60a8d292303cc80dab1f11407eb
WalterxWhite <guilain.savry@outlook.com>
2015-12-04 08:39:48 +0100

    Update Trie_baleine.py

--- Trie_baleine.py
+++ Trie_baleine.py
@@ -4,4 +4,9 @@ from scipy import stats
 from math import sqrt
 from sklearn import linear_model
 
-def
+def ouverture ( ):
+  csv = np.genfromtxt ('train.csv', delimiter=",")
+  clip_name = csv[:,0]
+  label = csv[:,1]
+
+  
