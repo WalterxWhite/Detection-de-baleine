82c0ebba4b3413a76b0d63f0f3ede071f64b7fe8
WalterxWhite <guilain.savry@outlook.com>
2015-12-11 09:14:40 +0100

    Update Squelette.py

--- Squelette.py
+++ Squelette.py
@@ -1,4 +1,4 @@
-    " I partie Transformation des fichiers audio "
+" I partie Transformation des fichiers audio "
     
     " Séparation des fichiers train en deux "
         " X_train, X_test, y_train, y_test = train_test_split("
@@ -8,11 +8,11 @@
     " Effectuer une rFFT(Fast Fourier Transform) discrète de dimension 1 sur les train"
     " Prend que les premiers 1000kHz du fichier et enregistrement dans un vecteur numpy " 
 
-    " II partie : Etalonnage de nos données"
+" II partie : Etalonnage de nos données"
 
     " Utilisation Scikit learn et attribution des 1/0 pour les trains "
 
-    " III partie : Attribution des 1/0 pour les test ( avec % d'erreur ) "
+" III partie : Attribution des 1/0 pour les test ( avec % d'erreur ) "
 
     " Comparation des fichiers Test avec notre base de données"
     " Enregistrer le tout dans un tableau "
