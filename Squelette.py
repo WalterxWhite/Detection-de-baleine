95e0d6dea71e65aa3c1b8fcf9d484f99ec549250
WalterxWhite <guilain.savry@outlook.com>
2015-12-11 09:14:06 +0100

    Update Squelette.py

--- Squelette.py
+++ Squelette.py
@@ -1,6 +1,9 @@
     " I partie Transformation des fichiers audio "
     
-    " Lire les differents fichier audio ' Train/Test ' à la chaine "
+    " Séparation des fichiers train en deux "
+        " X_train, X_test, y_train, y_test = train_test_split("
+        " X, y, test_size=0.33, random_state=42) "
+    " Lire les differents fichier audio ' Train/test ' à la chaine "
     " Utiliser le module AIFC sur les fichiers "
     " Effectuer une rFFT(Fast Fourier Transform) discrète de dimension 1 sur les train"
     " Prend que les premiers 1000kHz du fichier et enregistrement dans un vecteur numpy " 
@@ -13,3 +16,4 @@
 
     " Comparation des fichiers Test avec notre base de données"
     " Enregistrer le tout dans un tableau "
+    " Comparaison des résultats avec les 1/0 "
